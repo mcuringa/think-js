df402801b7356cbe056858670842f882d52e1680
mxc <matt@curinga.com>
2018-11-15 23:04:03 -0500

    updated listings.js to be more generic

--- docs/js/listings.js
+++ docs/js/listings.js
@@ -1,38 +1,27 @@
 
-const makeListings = ()=> {
-
-  console.log("making code listings");
+const caseStudy = (container)=> {
 
   const alignNote = (note)=> {
     const n = note.dataset.lineNumber;
-    console.log("line number", n);
     const line = lines[n-1];
     alignToTop(note, line, container);
-
   }
-  let container = document.querySelector("section.tipCalculator");
-  let notes = document.querySelectorAll(".tipCalculator aside");
-  const lines = document.querySelectorAll(".tipCalculator .listingSrcCode .sourceLine");
-  console.log("lines", lines);
+
+  let notes = container.querySelectorAll("aside");
+  const lines = container.querySelectorAll(".listingSrcCode .sourceLine");
   notes = notes.forEach(alignNote);
-  
 }
 
 
 const alignToTop = (note, line, container)=> {
-
-  console.log("container: ", container);
-  console.log("aligning note: ", note);
-  console.log("aligning to: ", line);
   const containerTop = container.getBoundingClientRect().top;
   const lineTop = line.getBoundingClientRect().top;
   const off = lineTop - containerTop;
-  console.log(off);
   note.style.top = off + "px";
-
-
-
-
 }
 
+const makeListings = ()=> {
+  let listings = document.querySelectorAll(".codeNotes");
+  listings.forEach(caseStudy);
+}
 makeListings();
